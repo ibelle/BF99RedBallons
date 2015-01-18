5569a484528d0858f634260a0aec627c7e2c6086
HUGE | Isaiah Belle <ibelle@ibelle-ny-05589.home>
2015-01-18 01:30:35 -0500

    Completed Challenge
    Completed Challenge

--- 99RedBalloons/ViewController.swift
+++ 99RedBalloons/ViewController.swift
@@ -11,10 +11,19 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var balloonImageView: UIImageView!
     @IBOutlet weak var balloonLabel: UILabel!
+    var currentBalloonIndex:Int=0
+    var myBalloons: [Balloon] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myBalloons = buildBalloonArray()
+        println(myBalloons)
+        for (balloon) in myBalloons {
+            println("\(balloon))")
+        }
+        
+        setCurrentBalloon(myBalloons.first!)
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +33,54 @@ class ViewController: UIViewController {
 
 
     @IBAction func nextBalloonButtonPressed(sender: UIBarButtonItem) {
+        var randomNumber:Int
+        do{
+            randomNumber = Int(arc4random_uniform(UInt32(myBalloons.count)))
+        } while randomNumber == currentBalloonIndex
+        currentBalloonIndex = randomNumber
+        var nextBalloon = myBalloons[currentBalloonIndex]
+        
+        UIView.transitionWithView(self.view, duration: 1, options: UIViewAnimationOptions.TransitionCrossDissolve,
+            animations: {
+                self.setCurrentBalloon(self.myBalloons[randomNumber])
+            },
+            completion: {
+                (finished: Bool) -> () in
+        })
+
+    }
+    
+    
+    func buildBalloonArray() -> [Balloon]{
+        var balloonCollection :[Balloon] = [Balloon]()
+        for  var i=1; i<100; i++ {
+            
+            var newBalloon = Balloon()
+            newBalloon.number = i
+            let balloonNum = 1 + Int(arc4random_uniform(UInt32(5)))
+            
+            switch balloonNum {
+            case 1:
+                newBalloon.image = UIImage(named: "RedBalloon1.jpg")
+            case 2:
+               newBalloon.image = UIImage(named: "RedBalloon2.jpg")
+            case 3:
+                newBalloon.image = UIImage(named: "RedBalloon3.jpg")
+            case 4:
+                newBalloon.image = UIImage(named: "RedBalloon4.jpg")
+            default:
+                newBalloon.image = UIImage(named: "BerlinTVTower.jpg")
+            }
+            balloonCollection += [newBalloon]
+        }
+        return balloonCollection
+    }
+    
+    
+    func setCurrentBalloon(myBalloon: Balloon){
+        balloonLabel.text = "Now Showing Balloon #\(myBalloon.number)"
+        balloonLabel.hidden = false
+        balloonImageView.image = myBalloon.image
     }
 }
 
